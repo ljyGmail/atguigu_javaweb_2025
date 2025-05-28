fa0ab23fc346e38d06da2a71509b4918d40b9329
ljyGmail <95560385+ljyGmail@users.noreply.github.com>
2025-05-28 13:12:53 +0900

    59 登录验证展示左侧好友列表

---
+++ pro15-project-qqzone/src/main/java/com/atguigu/myssm/myspringmvc/PageController.java
@@ -0,0 +1,17 @@
+package com.atguigu.myssm.myspringmvc;
+
+/**
+ * ClassName: PageController
+ * Package: com.atguigu.myssm.myspringmvc
+ * Description:
+ *
+ * @Author ljy
+ * @Create 2025. 5. 28. 오후 1:04
+ * @Version 1.0
+ */
+public class PageController {
+
+    public String page(String page) {
+        return page;
+    }
+}
